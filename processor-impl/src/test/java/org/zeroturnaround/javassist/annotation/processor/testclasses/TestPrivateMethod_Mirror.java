b106914f71555061f710ea35d339095b05a336da
Дмитрий А. Елисеев <eliseev_d@ntcees.ru>
2020-09-22 12:51:25 +0300

    Fix source for tests result compare

--- processor-impl/src/test/java/org/zeroturnaround/javassist/annotation/processor/testclasses/TestPrivateMethod_Mirror.java
+++ processor-impl/src/test/java/org/zeroturnaround/javassist/annotation/processor/testclasses/TestPrivateMethod_Mirror.java
@@ -1,9 +1,7 @@
 package org.zeroturnaround.javassist.annotation.processor.testclasses;
 
 public class TestPrivateMethod_Mirror {
-  protected final void instrument(org.zeroturnaround.javassist.annotation.MethodCall call) {}
-public TestPrivateMethod_Mirror(
-) {  super();}
-protected java.lang.String privateMethod(java.lang.String $1){
-return null;
-}}
+protected final void instrument(org.zeroturnaround.javassist.annotation.MethodCall call) {}
+public TestPrivateMethod_Mirror() {super();}
+protected java.lang.String privateMethod(java.lang.String $1){ return null; }
+}
--- processor-impl/src/test/java/org/zeroturnaround/javassist/annotation/processor/testclasses/TestSimple_Mirror.java
+++ processor-impl/src/test/java/org/zeroturnaround/javassist/annotation/processor/testclasses/TestSimple_Mirror.java
@@ -1,7 +1,6 @@
 package org.zeroturnaround.javassist.annotation.processor.testclasses;
 
 public class TestSimple_Mirror {
-  protected final void instrument(org.zeroturnaround.javassist.annotation.MethodCall call) {}
-public TestSimple_Mirror(
-) {  super();}
+protected final void instrument(org.zeroturnaround.javassist.annotation.MethodCall call) {}
+public TestSimple_Mirror() {super();}
 }
